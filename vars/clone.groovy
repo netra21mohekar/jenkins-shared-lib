121fd1efc3c62ce98a14e1d1c99c81c4f4be94b3
netra21mohekar <148302146+netra21mohekar@users.noreply.github.com>
2025-08-16 18:06:25 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
-def clone(String url,String branch){
-  echo "clone the git repo"
-  git url: ${url} branch:${branch}
-  echo "cloned repo succesfully"
+def call(String url, String branch) {
+    echo "Cloning the git repo..."
+    git branch: branch, url: url
+    echo "Cloned repo successfully!"
 }
